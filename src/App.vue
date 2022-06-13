f320fd756f5eef2cee28fabe831287a37f059812
Tim A <tim.askew@walsworth.com>
2022-06-13 13:04:14 -0500

    Style-Update

--- src/App.vue
+++ src/App.vue
@@ -112,7 +112,7 @@ export default {
 </script>
 
 <style>
-img{
+.logo{
   max-height: 20%;
   max-width: 20%; 
 }
--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,31 +1,33 @@
 <template>
-
- <div class="parentColumn">
+<div class="inner-container ">
+ <div class="portfolio-grid">
    
-  <ul class="images imageSet">
+  <ul class="images imageSet grid gap-4 grid-cols-3 grid-rows-3">
     <li
       v-for="image in images"
       v-bind:key="image.sys.id"
-      class="image"
-    > 
+      class="image my-4"
+    >
+
       <img :src="image.galleyImage.url" />
+        
         <div class="image-info">
-
             <img :src="image.galleryImage" />
-
-         <div class="image-name">
-           <div class="">{{ image.imageTitle }}
-          test</div>
-         </div>
-         <div class="image-content">
-           {{ image.postTime }}
-         </div>
+            <div class="inline-block">
+                  <div class="image-name">
+                    <div class="">{{ image.imageTitle }}
+                    test</div>
+                  </div>
+                  <div class="image-content">
+                    {{ image.postTime }}
+                  </div>
+            </div>
        </div>
 
     </li>
   </ul>
   </div>
-
+</div>
 </template>
 
 
@@ -87,10 +89,7 @@ export default {
 </script>
 
 <style>
-img{
-  max-height: 20%;
-  max-width: 20%; 
-}
+
 
 body {
   margin: 0;
@@ -99,30 +98,6 @@ body {
   color: black;
 }
 
-/* @import "@/assets/styles/variables.scss";
-* {
-  box-sizing: border-box;
-}
-
-#app {
-  background-color: $purple;
-  border: 7px solid $black;
-  box-shadow: 10px 10px 0 $black;
-  border-radius: 7px 7px 7px 0;
-  max-width: 1200px;
-  margin: 50px auto;
-  @media screen and (max-width: 1200px) {
-    margin: 20px;
-  }
-}
-ul {
-  margin: 0;
-  list-style: none;
-  padding: 0;
-  li {
-    display: inline-block;
-  }
-} */
 .portfolio-grid {
   display: grid;
   margin-top: var(--section-spacing-2);
@@ -132,19 +107,30 @@ ul {
   height: 100%;
 }
 
+.portfolio-grid-item {
+  grid-row: span 2;
+  grid-column: span 2;
+}
+
+.inner-container{
+  max-width: 1120px;
+  margin-left: auto;
+  margin-right: auto;
+}
 
 .image{
   display: inline-block;
   width: 300px;
+  background-color: white;
 }
 
 .parentColumn {
     width: 100%;
     max-width: 1200px;
     display: grid;
-    margin-top: var(--section-spacing-2);
+    /* margin-top: var(--section-spacing-2); */
     grid-template: 1fr / 1fr;
-    grid-gap: var(--section-spacing-1);
+    /* grid-gap: var(--section-spacing-1); */
     width: 100%;
     height: 100%;
     }
@@ -152,17 +138,18 @@ ul {
     margin: 0;
     list-style: none;
     padding: 0;
-     
     };
+    
   .imageSet ::after {
     display: inline-block;
     display: grid;
         /* margin-top: 30px; */
-    margin-top: var(--section-spacing-2);
+    /* margin-top: var(--section-spacing-2); */
     grid-template: 2fr 2fr / 2fr 2fr;
     grid-template-rows: 2fr 2fr;
     grid-template-columns: 2fr 2fr;
     grid-template-areas: none;
     }
+    
     
 </style>
